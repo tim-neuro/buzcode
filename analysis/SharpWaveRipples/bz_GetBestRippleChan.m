24f9189a5b8ad851893d618620f1fccb1d92d5fa
Tim Eilers <eilers.tim1412@gmail.com>
2025-11-26 17:01:15 -0500

    bug fixes

--- analysis/SharpWaveRipples/bz_GetBestRippleChan.m
+++ analysis/SharpWaveRipples/bz_GetBestRippleChan.m
@@ -1,22 +1,37 @@
-function  [chan] = bz_GetBestRippleChan(lfp, n_channels, sampling_rate)
-%[chan] = bz_GetBestRippleChan(lfp)
-%eventually this will detect which lfp channel has the highest SNR for the
-% ripple componenent of SPWR events....
-
+function  [chan] = bz_GetBestRippleChan(lfp, hpc_channels, sampling_rate)
+%Detect which hpc lfp channel has the highest SNR for the
+% ripple componenent of SPWR events. returns 0 based channel idx!!
+n_channels = length(lfp.channels);
+mRipple = zeros(1, n_channels);
+meRipple = zeros(1, n_channels);
+mmRippleRatio = zeros(1, n_channels);
 % filters 140-180hz - uses normalized frequencies, where 1.0 = Nyquist frequency = sampling_rate/2.
-[b a]=butter(4,[140/(sampling_rate/2) 180/(sampling_rate/2)],'bandpass');
+[b, a]=butter(4,[140/(sampling_rate/2) 180/(sampling_rate/2)],'bandpass');
 
-for i=1:n_channels
-    filt = FiltFiltM(b,a,single(lfp(:,i)));
+% check on a single channel that filtering worked - if not, use lower order
+% filter
+filt_test = FiltFiltM(b,a,single(lfp.data(:,1)));
+if sum(isnan(filt_test)) > 100
+    [b, a]=butter(3,[140/(sampling_rate/2) 180/(sampling_rate/2)],'bandpass');
+    disp('Lower filter order had to be used to find best ripple channel')
+end
+% check if it still failed
+filt_test = FiltFiltM(b,a,single(lfp.data(:,1)));
+if sum(isnan(filt_test)) >100 
+    error('Filter failed when searching for best ripple channel')
+end
+for i=1:length(hpc_channels)
+    chan_idx = hpc_channels(i);
+    filt = FiltFiltM(b,a,single(lfp.data(:,chan_idx)));
     pow = fastrms(filt,15);    
-    mRipple(i) = mean(pow);
-    meRipple(i) = median(pow);
-    mmRippleRatio(i) = mRipple(i)./meRipple(i);
+    mRipple(chan_idx) = mean(pow);
+    meRipple(chan_idx) = median(pow);
+    mmRippleRatio(chan_idx) = mRipple(chan_idx)./meRipple(chan_idx);
 end
 
 mmRippleRatio(mRipple<1) = 0;
 mmRippleRatio(meRipple<1) = 0;
 
-[minVal loc] = max(mmRippleRatio);
+[~, loc] = max(mmRippleRatio);
 chan = lfp.channels(loc);
 end
